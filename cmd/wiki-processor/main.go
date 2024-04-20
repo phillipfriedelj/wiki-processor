7767a6270afae9550c0b30d795492a60dadda6c5
Phillip  Friedel <s0557917@htw-berlin.de>
2024-04-20 16:24:01 +0200

    Started with simple cli implementation

---
+++ cmd/wiki-processor/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/phillipfriedelj/wiki-processor/cmd/internal/cli"
+
+func main() {
+
+	command := cli.ParseCommandLineArgs()
+
+	command.Validate()
+	command.Run()
+}
